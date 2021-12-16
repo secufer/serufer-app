739ef12acb13ac8e8a96944b24b156b62eb9dfad
Prathmesh-Dhatrak <prathmesh101dhatrak@gmail.com>
2021-12-16 19:39:13 +0530

    first commit

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import Background from '../components/Background'
-import Logo from '../components/Logo'
-import Header from '../components/Header'
-import Paragraph from '../components/Paragraph'
-import Button from '../components/Button'
+import React from "react";
+import Background from "../components/Background";
+import Logo from "../components/Logo";
+import Header from "../components/Header";
+import Paragraph from "../components/Paragraph";
+import Button from "../components/Button";
 
 export default function Dashboard({ navigation }) {
   return (
     <Background>
       <Logo />
-      <Header>Let’s start</Header>
+      <Header>Secufer</Header>
       <Paragraph>
         Your amazing app starts here. Open you favorite code editor and start
         editing this project.
@@ -19,12 +19,12 @@ export default function Dashboard({ navigation }) {
         onPress={() =>
           navigation.reset({
             index: 0,
-            routes: [{ name: 'StartScreen' }],
+            routes: [{ name: "StartScreen" }],
           })
         }
       >
         Logout
       </Button>
     </Background>
-  )
+  );
 }
